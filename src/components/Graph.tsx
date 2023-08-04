cf0f8b74df54de96392158b2a6178242bc102112
JinxinZhao315 <34888060+JinxinZhao315@users.noreply.github.com>
2023-08-04 18:43:43 +0800

    React基本框架

---
+++ src/components/Graph.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Graph.css';
+
+export default function Graph() {
+    return (
+        <div id="graph-pane">
+            <div id="graph-display">
+                This is graph.
+            </div>
+        </div>
+    )
+
+}
