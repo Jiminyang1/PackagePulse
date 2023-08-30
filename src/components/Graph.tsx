2b8ef00261e049fc812d7a34c3d9185f9d24f76f
huanxiqiao <huanxiqiao@qq.com>
2023-08-30 22:46:51 +0800

    Graph.tsx update

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,31 +2,29 @@ import React, { useEffect, useRef, useState } from 'react'
 import * as d3 from 'd3'
 import './styles/Graph.css'
 
-// 定义层次化数据结构类型
 interface HierarchyNode {
   name: string // 节点名称
-  version: number // 版本号
+  version: string // 版本号
   children: HierarchyNode[] // 子节点
   hasCircularDependency?: boolean // 标记循环依赖
   hasDuplicateDependency?: boolean // 标记重复依赖
+  totalDependencies: number // 依赖总数
 }
 
-// 将扁平化数据转换为层次化数据
 function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
   const root: HierarchyNode = {
     name: data.name,
     version: data.version,
-    children: []
+    children: [],
+    totalDependencies: 0
   }
 
-  // 初始化循环依赖和重复依赖标记
   let hasCircularDependency = false
   let hasDuplicateDependency = false
 
   if (data.dependecies && data.dependecies.length > 0) {
-    const dependencyCountMap = new Map() // 记录依赖项出现次数的 Map
+    const dependencyCountMap = new Map()
     data.dependecies.forEach((dep) => {
-      // 检查依赖项是否已经出现过
       if (dependencyCountMap.has(dep.name)) {
         dependencyCountMap.set(dep.name, dependencyCountMap.get(dep.name) + 1)
         hasDuplicateDependency = true
@@ -34,7 +32,6 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
         dependencyCountMap.set(dep.name, 1)
       }
 
-      // 判断循环依赖
       if (ancestors.includes(dep.name)) {
         hasCircularDependency = true
       }
@@ -44,10 +41,15 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
       const childNode = convertHierarchy(dep, ancestors)
       root.children.push(childNode)
 
+      if (childNode.children.length === 0) {
+        root.totalDependencies += 1
+      } else {
+        root.totalDependencies += childNode.totalDependencies
+      }
+
       ancestors.pop()
     })
 
-    // 设置循环依赖和重复依赖属性
     root.hasCircularDependency = hasCircularDependency
     root.hasDuplicateDependency = hasDuplicateDependency
   }
@@ -55,97 +57,164 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
   return root
 }
 
-// 合并多个层次化数据对象为一个
 function mergeHierarchyData(dataObjects): HierarchyNode {
   const mergedRoot: HierarchyNode = {
-    name: 'mergedRoot',
-    version: 0,
-    children: []
+    name: 'Project Name: ##########',
+    version: '=====',
+    children: [],
+    totalDependencies: 0
   }
+
   dataObjects.forEach((data) => {
     const hierarchyData = convertHierarchy(data)
     mergedRoot.children.push(hierarchyData)
+    mergedRoot.totalDependencies += hierarchyData.totalDependencies
   })
   return mergedRoot
 }
 
-// 创建 SVG 元素
-function createSvg() {
-  const svg = d3
-    .select('#graph-svg')
-    .attr('width', '100%')
-    .attr('height', '100%')
-  const width = svg.node().getBoundingClientRect().width
-  const height = svg.node().getBoundingClientRect().height
-  return { svg, width, height }
-}
+function drawNodelinks(g, hierarchyData) {
+  const format = d3.format(',')
+  const nodeSize = 20
+  const root = d3.hierarchy(hierarchyData).eachBefore(
+    (
+      (i) => (d) =>
+        (d.index = i++)
+    )(0)
+  )
+  const nodes = root.descendants()
 
-//绘制节点链接图
-function drawNodelinks(svg, width, height, hierarchyData) {
-  const nodelinkG = svg
+  const columns = [
+    {
+      label: 'NPM PACKAGE ANALYZER',
+      value: (d) => d.version,
+      format,
+      x: 150,
+      render: null
+    },
+    {
+      label: 'version',
+      value: (d) => d.version,
+      format,
+      x: 500,
+      render: (text) => text.data.version
+    },
+    {
+      label: 'dependencies',
+      value: (d) => d.hasCircularDependency,
+      format,
+      x: 600,
+      render: (text) => text.data.totalDependencies
+    }
+  ]
+
+  const link = g
     .append('g')
-    .attr('class', 'nodelinkG')
-    .attr('transform', `translate(30,10)`)
-  const treeLayout = d3.tree().size([height - 50, width / 1.2])
-  const root = d3.hierarchy(hierarchyData)
-  const links = treeLayout(root).links()
-  const nodes = treeLayout(root).descendants()
-
-  const link = nodelinkG
-    .selectAll('path')
-    .data(links)
+    .attr('class', 'links')
+    .selectAll()
+    .data(root.links())
     .join('path')
+    .attr('fill', 'none')
+    .attr('stroke', '#8758ff')
     .attr(
       'd',
-      d3
-        .linkHorizontal()
-        .x((d) => d.y)
-        .y((d) => d.x)
+      (d) => `
+        M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+        V${d.target.index * nodeSize}
+        h${nodeSize}
+      `
     )
-    .attr('fill', 'none')
-    .attr('stroke', 'grey')
-    .attr('stroke-width', 0.5)
 
-  const nodeWidth = d3.max(nodes, (d) => d.data.name.length * 8)
-
-  const node = nodelinkG
-    .selectAll('rect')
+  const node = g
+    .append('g')
+    .attr('class', 'nodes')
+    .selectAll()
     .data(nodes)
-    .join('circle')
-    .attr('cx', (d) => d.y)
-    .attr('cy', (d) => d.x)
+    .join('g')
+    .attr('transform', (d) => `translate(0,${d.index * nodeSize})`)
+
+  node
+    .append('circle')
+    .attr('cx', (d) => d.depth * nodeSize)
     .attr('r', 3)
-    .attr('fill', (d) => (d.data.hasCircularDependency ? 'red' : '#ccc'))
-    .attr('stroke', 'black')
-    .attr('stroke-width', 1)
-    .attr('stroke-opacity', 0.5)
+    .attr('fill', (d) => (d.children ? '#8758ff' : '#aac4ff'))
 
-  const label = nodelinkG
-    .selectAll('text')
-    .data(nodes)
-    .join('text')
-    .attr('dy', '0.31em')
-    .attr('x', (d) => d.y + 4)
-    .attr('y', (d) => d.x)
+  node
+    .append('rect')
+    .attr('x', (d) => d.depth * nodeSize + 6)
+    .attr('y', -nodeSize / 2 + 3)
+    .attr('width', (d) => d.data.name.length * 7.5)
+    .attr('height', 15)
+    .attr('fill', '#d2daff')
+    .attr('stroke', 'black')
+    .attr('rx', 5)
+    .attr('ry', 5)
+    .attr('opacity', (d) => (d.depth == 0 ? 0 : 0.3))
+
+  node
+    .append('text')
+    .attr('dy', '0.32em')
+    .attr('x', (d) => d.depth * nodeSize + 8)
     .text((d) => d.data.name)
-    .attr('font-size', 8)
-    .attr('fill', 'black')
-    .attr('text-anchor', 'start')
+    .attr('fill', (d) => (d.data.version == '*' ? '#8758ff' : '#555'))
+    .attr('font-size', (d) => (d.depth == 0 ? '13px' : '12px'))
+    .attr('font-weight', (d) => (d.depth == 0 ? 'bold' : 'normal'))
+
+  node.append('title').text((d) =>
+    d
+      .ancestors()
+      .reverse()
+      .map((d) => d.data.name)
+      .join('/')
+  )
+
+  for (const { label, value, format, x, render } of columns) {
+    g.append('text')
+      .attr('dy', '0.32em')
+      .attr('y', -nodeSize)
+      .attr('x', x)
+      .attr('text-anchor', 'end')
+      .attr('font-weight', 'bold')
+      .attr('font-size', '12px')
+      .attr('fill', '#555')
+      .text(label)
+
+    node
+      .append('text')
+      .attr('dy', '0.32em')
+      .attr('x', x)
+      .attr('text-anchor', 'end')
+      .attr('fill', '#555')
+      .data(root.copy().sum(value).descendants())
+      .attr('font-size', '12px')
+      .text(render)
+  }
+
+  return g.node()
 }
 
 export default function Graph() {
   const svgRef = useRef(null)
+  const gRef = useRef(null)
   const [jsonData, setJsonData] = useState([])
 
   useEffect(() => {
-    const { svg, width, height } = createSvg()
+    const svgElement = d3
+      .select(svgRef.current)
+      .attr('width', '100%')
+      .attr('height', '100%')
+
+    const gElement = svgElement
+      .append('g')
+      .attr('transform', `translate(${20},${40})`)
+    gRef.current = gElement
 
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3030')
+        // const response = await fetch('http://localhost:3030')
+        const response = await fetch('./result.json')
         const data = await response.json()
 
-        // 将多个数据对象合并为一个层次化数据对象
         const mergedHierarchyData = mergeHierarchyData([
           data[0],
           data[1],
@@ -154,8 +223,7 @@ export default function Graph() {
           data[4]
         ])
 
-        // console.log(mergedHierarchyData)
-        drawNodelinks(svg, width, height, mergedHierarchyData)
+        drawNodelinks(gElement, mergedHierarchyData)
 
         setJsonData(data)
 
@@ -166,7 +234,6 @@ export default function Graph() {
       }
     }
     fetchData()
-
   }, [])
 
   return (
